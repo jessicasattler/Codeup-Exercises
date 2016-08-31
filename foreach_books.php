2d59e7de7d701df9cd830ff5c74d99780279d979
Jessica Sattler <jessicasattler28@gmail.com>
2016-08-31 15:41:19 -0500

    Created "foreach_books.php" file and I can access the information inside the array of each book, but I still can't access the title

---
+++ foreach_books.php
@@ -0,0 +1,35 @@
+<?php 
+
+
+$books = array(
+    'The Hobbit' => array(
+        'published' => 1937,
+        'author' => 'J. R. R. Tolkien',
+        'pages' => 310
+    ),
+    'Game of Thrones' => array(
+        'published' => 1996,
+        'author' => 'George R. R. Martin',
+        'pages' => 835
+    ),
+    'The Catcher in the Rye' => array(
+        'published' => 1951,
+        'author' => 'J. D. Salinger',
+        'pages' => 220
+    ),
+    'A Tale of Two Cities' => array(
+        'published' => 1859,
+        'author' => 'Charles Dickens',
+        'pages' => 544
+    )
+);
+// Construct a loop that iterates through each book and then each book's keys and values. Have it output the book's title, then list the key value pairs for the data about each book.
+
+foreach ($books as $book){
+	foreach ($book as $key => $value){
+		echo "Books - $key : $value\n";
+	}
+}
+
+
+// Update your loop to only show books that were written after 1950
