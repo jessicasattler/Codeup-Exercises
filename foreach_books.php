39f8ec7c7d0bd8128b52519a0022710180266190
Jessica Sattler <jessicasattler28@gmail.com>
2016-08-31 20:11:58 -0500

    foreach_books works, including the second exercise. Multiple ways of getting at the right answer are shown.

--- foreach_books.php
+++ foreach_books.php
@@ -25,11 +25,29 @@ $books = array(
 );
 // Construct a loop that iterates through each book and then each book's keys and values. Have it output the book's title, then list the key value pairs for the data about each book.
 
-foreach ($books as $book){
-	foreach ($book as $key => $value){
+
+	foreach ($books as $key => $value){
+		echo "$key" .PHP_EOL;
+		foreach ($value as $key => $value){
 		echo "Books - $key : $value\n";
+
+		
+		}
 	}
-}
+echo "New Exercise with Demo Way".PHP_EOL.PHP_EOL;
+//or Ryan's way
+	foreach($books as $title => $book){
+		echo "$title was written by {$book['author']}, it was published in {$book["published"]}, and it contains {$book['pages']} pages".PHP_EOL;
+	}
 
 
 // Update your loop to only show books that were written after 1950
+echo "New Exercise".PHP_EOL.PHP_EOL;
+foreach ($books as $key => $value){
+	//book was written after 1950 , echo the name of the book 
+	if (($value ["published"]) > 1950){
+		
+			echo $key . PHP_EOL;
+		
+	}
+}
